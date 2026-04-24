c5ac4b2046a27465e4087c56e38b11c0521a84a9
joyhandenergy <joyhandenergy@gmail.com>
2026-04-24 20:02:59 +0800

    new add

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,6 @@
-// open-next.config.js
-/** @type {import('@opennextjs/cloudflare').OpenNextConfig} */
-const config = {
+import type { OpenNextConfig } from "@opennextjs/cloudflare";
+
+const config: OpenNextConfig = {
   default: {
     override: {
       wrapper: "cloudflare-node",
